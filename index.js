8441d7492b34eba0621747b4d3ba2d3abd014841
Mateusz Choma <kontakt@coderoad.pl>
2021-01-07 19:52:08 +0100

    Deleted DecreasingCounter

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import { Counter } from './Counter.js'
-import { DecreasingCounter } from './DecreasingCounter.js'
 
 const counter1 = new Counter('.counter-1')
 counter1.init()
-const counter2 = new DecreasingCounter('.counter-2')
-counter2.init()
